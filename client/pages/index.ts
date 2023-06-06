d497307c0915cee0235d50eafe6a9e09316fde6a
Susie Son <susie@stytch.com>
2023-06-06 14:49:52 -0700

    new managed workflow app

--- client/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProfilePage } from "./ProfilePage";
-export { default as SendOTPPage } from "./SendOTPPage";
-export { default as VerifyOTPPage } from "./VerifyOTPPage";
-export { default as WelcomePage } from "./WelcomePage";
