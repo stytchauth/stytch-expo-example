6dd4743b7bd2d877e3470f4c9bfdc4a0df28edaf
Susie Son <susie@stytch.com>
2023-06-23 13:37:41 -0700

    remove eas build project info, add inappbrowser fork

--- app.config.js
+++ app.config.js
@@ -4,9 +4,6 @@ export default ({ config }) => {
   return {
     ...config,
     extra: {
-      eas: {
-        projectId: "9c5f8103-6b40-412c-b797-6a20c969b9d5",
-      },
       stytchPublicToken: process.env.STYTCH_PUBLIC_TOKEN,
     },
   };
